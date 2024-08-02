1476c65136ff34be472632557fbb6febd712a2c1
matinmehrdadi <matin.md.2004.9@gmail.com>
2024-08-03 02:10:22 +0330

    b

--- cart-ts/src/components/productItem/ProductItem.tsx
+++ cart-ts/src/components/productItem/ProductItem.tsx
@@ -1,11 +1,16 @@
 function ProductItem() {
   return (
     <div className="transition rounded-lg">
-      <div className="transition-all  h-80  hover:border-neutral-900  bg-neutral-700  hover:bg-black  rounded-lg ">
+      <div className="transition-all  text-right pb-2 border-2 hover:border-2 border-neutral-700 hover:text-white text-white hover:border-neutral-900  bg-neutral-700  hover:bg-black active:bg-neutral-700 rounded-lg ">
         <img
           src="src/assets/images/Screenshot_20230624_215528_BolBol Jan.jpg"
-          className="h-60 w-full rounded-t-lg"
+          className="h-40 w-full rounded-t-lg "
         />
+        <p className="p-2 flex text-left">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+        </p>
+        <div className="pr-4 pb-2">نامحصول</div>
+        <span className=" pr-4">قیمت: $22</span>
       </div>
     </div>
   );
--- cart-ts/src/pages/store/Store.tsx
+++ cart-ts/src/pages/store/Store.tsx
@@ -3,8 +3,8 @@ import ProductItem from "../../components/productItem/ProductItem";
 function Store() {
   return (
     <div className="pt-20 ">
-      <p className="text-white px-28 text-right mb-5">محصولات جدید</p>
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5  px-28">
+      <p className="text-white px-28 text-right mb-5 text-xl">محصولات جدید</p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4  gap-5  px-28">
         <ProductItem />
         <ProductItem />
         <ProductItem />
