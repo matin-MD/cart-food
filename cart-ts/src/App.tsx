5be6d0d4b43c37c34dbd99699f06b89dcdf30282
matin.md <matin.md.2004.9@gmail.com>
2024-08-04 05:18:45 +0330

    5:50 - 9

--- cart-ts/src/App.tsx
+++ cart-ts/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Store from "./pages/store/Store";
 import Layout from "./components/layout/Layout";
 import Product from "./pages/product/Product";
+import Cart from "./pages/home/cart/Cart";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
-        <Route path="/product/:id" element={<Product/>}/>
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Layout>
   );
--- cart-ts/src/pages/store/Store.tsx
+++ cart-ts/src/pages/store/Store.tsx
@@ -1,14 +1,23 @@
+import { Link } from "react-router-dom";
 import ProductItem from "../../components/productItem/ProductItem";
 
 function Store() {
   return (
-    <div className="pt-20 ">
+    <div className="pt-10 ">
       <p className="text-white px-28 text-right mb-5 text-xl">محصولات جدید</p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4  gap-5  px-28">
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4  gap-5   px-28">
+        <Link to={`/product/${1}`} className="pb-10">
+          <ProductItem />
+        </Link>
+        <Link to={`/product/${1}`} className="pb-10">
+          <ProductItem />
+        </Link>
+        <Link to={`/product/${1}`} className="pb-10">
+          <ProductItem />
+        </Link>
+        <Link to={`/product/${1}`} className="pb-10">
+          <ProductItem />
+        </Link>
       </div>
     </div>
   );
