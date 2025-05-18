ae7539b2199053e7f6c327b55dff499033296799
matinmehrdadi <139682537+matinmehrdadi@users.noreply.github.com>
2025-05-18 06:08:21 +0330

    .

--- cart-ts/src/pages/store/Store.tsx
+++ cart-ts/src/pages/store/Store.tsx
@@ -15,7 +15,7 @@ function Store() {
       <p className="text-white px-28 mb-5 text-xl text-center sm:text-right md:text-right lg:text-right">
         محصولات جدید
       </p>
-      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 xl:gap-14 2xl:gap-24 gap-5  px-28">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 xl:gap-14 2xl:gap-24 gap-5 px-28">
         {product.map((item) => (
           <Link key={item.id} to={`/product/${item.id}`} className="pb-10">
             <ProductItem {...item} />
